bb7d2a1abb3a5eeb527df2f9d64f4dd55a73ae92
Son of Isaac <isaacmulemfwe@gmail.com>
2019-06-12 18:03:31 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,7 @@
 // you solution should be here
+var birth_year = 1996;
+var future_year = 2029;
+var present_year = 2019;
+//Two possible ages
+var future_age = future_year - birth_year;
+var present_age = present_year - birth_year;
